54dbe226bf2bc05ceb9278464a9561ee9c3842a2
DKKs <eye.q.21@gmail.com>
2024-06-24 02:49:37 +0700

    [Add] init

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#edfaff",
+          100: "#d6f2ff",
+          200: "#b5eaff",
+          300: "#83dfff",
+          400: "#48cbff",
+          500: "#1eadff",
+          600: "#068fff",
+          700: "#007bff",
+          800: "#085ec5",
+          900: "#0d519b",
+          950: "#0e315d",
+        },
+        secondary: "#F8F8F8",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
