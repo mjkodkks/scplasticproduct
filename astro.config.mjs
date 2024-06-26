bd6dfe32823beacdf78e1baaf05b61ae57071ff5
Thanonphat.su (DKKs) <thanonphat.supho@krungsri.com>
2024-06-26 12:22:15 +0700

    [Add] product, social link

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: "https://www.scplasticproduct.com",
-  integrations: [tailwind(), mdx(), sitemap(), icon()],
+  integrations: [tailwind(), mdx(), sitemap(), icon(), partytown()],
   i18n: {
     defaultLocale: "th",
     locales: ["th", "en"],
