6a5b557d40c7df33ce45839a992e9d32342d0c68
Thanonphat.su (DKKs) <thanonphat.supho@krungsri.com>
2024-07-11 03:39:50 +0700

    [Add] content + project page

--- src/content/config.ts
+++ src/content/config.ts
@@ -40,10 +40,21 @@ const contactCollection = defineCollection({
   }),
 });
 
+const projectCollection = defineCollection({
+  schema: z.object({
+    projects: z.array(z.object({
+      title: z.string(),
+      description: z.string(),
+      image: z.string()
+    })),
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   'blog': blogCollection,
   'team': teamCollection,
   'contact': contactCollection,
+  'project': projectCollection
 };
